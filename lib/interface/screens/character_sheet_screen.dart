8448d9850f353a218775b12a7d3738953ce56222
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-02-13 21:57:13 +0100

    #3 - formatted new data

--- lib/interface/screens/character_sheet_screen.dart
+++ lib/interface/screens/character_sheet_screen.dart
@@ -17,20 +17,82 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
       appBar: AppBar(
         title: Center(child: Text(widget.character.name)),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
         children: <Widget>[
           Text("Race: ${widget.character.race.name}"),
           Text("Subrace: ${widget.character.subrace.toString()}"),
           Text("Size: ${widget.character.race.size}"),
           Text("Profession: ${widget.character.profession.name}"),
-          Text("Attributes: ${widget.character.attributes.toString()}"),
-          Text("Skills: ${widget.character.skills.toString()}"),
-          Text("Talents: ${widget.character.talents.toString()}"),
-          Text("Armour: ${widget.character.armour.toString()}"),
-          Text("Melee weapons: ${widget.character.meleeWeapons.toString()}"),
-          Text("Ranged weapons: ${widget.character.rangedWeapons.toString()}")
+          const Text("Attributes:"),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  for (var attribute in widget.character.attributes.values) Text(attribute.name)
+                ]
+              ),
+              TableRow(
+                children: [
+                  for (var attribute in widget.character.attributes.values) Text((attribute.base + attribute.advances).toString())
+                ]
+              )
+            ],
+          ),
+          const Text("Skills:"),
+          Table(
+            columnWidths: const {
+              0: FlexColumnWidth(),
+              1: FixedColumnWidth(48),
+              2: FixedColumnWidth(32)
+            },
+            children: [
+              for (var skill in widget.character.skills.values) TableRow(
+                children: [
+                  Text(skill.name),
+                  Text(skill.attribute!.name),
+                  Text((skill.attribute!.base + skill.attribute!.advances + skill.advances).toString()),
+                ]
+              ),
+            ],
+          ),
+          const Text("Talents:"),
+          Column(
+            children: [
+              for (var talent in widget.character.talents.values) Text(talent.name)
+            ],
+          ),
+          const Text("Armour:"),
+          Table(
+            columnWidths: const {
+              0: FlexColumnWidth(),
+              1: FixedColumnWidth(32),
+              2: FixedColumnWidth(32),
+              3: FixedColumnWidth(32),
+              4: FixedColumnWidth(32),
+              5: FixedColumnWidth(32),
+              6: FixedColumnWidth(32)
+            },
+            children: [
+              for (var armour in widget.character.armour) TableRow(
+                children: [
+                  Text(armour.name),
+                  Text(armour.headAP.toString()),
+                  Text(armour.bodyAP.toString()),
+                  Text(armour.leftArmAP.toString()),
+                  Text(armour.rightArmAP.toString()),
+                  Text(armour.leftLegAP.toString()),
+                  Text(armour.rightLegAP.toString())
+                ]
+              )
+            ],
+          ),
+          const Text("Weapons:"),
+          Column(
+            children: [
+              for (var weapon in widget.character.meleeWeapons) Text(weapon.name),
+              for (var weapon in widget.character.rangedWeapons) Text(weapon.name)
+            ],
+          )
         ]
       )
     );
