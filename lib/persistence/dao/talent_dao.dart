c3780d0468bd9dc22080eb500d7db6060602a4d7
Jakub Robaczewski <kubarob13@gmail.com>
2022-03-14 15:15:34 +0100

    #6 - Changed talents and split localisation into files

--- lib/persistence/dao/talent_dao.dart
+++ lib/persistence/dao/talent_dao.dart
@@ -12,23 +12,28 @@ class TalentDAO extends DAO<Talent> {
 
   @override
   Future<Talent> fromMap(Map<String, dynamic> map, [Map overrideMap = const {}]) async {
-    BaseTalent? baseTalent = map["BASE_TALENT"] == null ? null : await BaseTalentDAO().get(map["BASE_TALENT"]);
-    return Talent(
-        id: map['ID'],
-        name: map['NAME'],
-        attribute: attributes?[map["MAX_LVL"]],
-        constLvl: map['CONST_LVL'],
-        baseTalent: baseTalent,
-        grouped: map['GROUPED'] == 1);
+    BaseTalent? baseTalent =
+        map["BASE_TALENT"] == null ? null : await BaseTalentDAO(attributes).get(map["BASE_TALENT"]);
+    return Talent(id: map['ID'], name: map['NAME'], specialisation: map["SPECIALISATION"], baseTalent: baseTalent);
   }
 }
 
 class BaseTalentDAO extends DAO<BaseTalent> {
+  Map<int, Attribute>? attributes;
+
+  BaseTalentDAO([this.attributes]);
+
   @override
   get tableName => 'talents_base';
 
   @override
   BaseTalent fromMap(Map<String, dynamic> map, [Map overrideMap = const {}]) {
-    return BaseTalent(id: map['ID'], name: map['NAME'], description: map['DESCRIPTION'], source: map['SOURCE']);
+    return BaseTalent(
+        id: map['ID'],
+        name: map['NAME'],
+        description: map['DESCRIPTION'],
+        source: map['SOURCE'],
+        attribute: attributes?[map["MAX_LVL"]],
+        constLvl: map['CONST_LVL']);
   }
 }
--- lib/persistence/entities/talent.dart
+++ lib/persistence/entities/talent.dart
@@ -4,15 +4,44 @@ import 'package:battle_it_out/persistence/entities/dto.dart';
 class Talent extends DTO {
   int id;
   String name;
-  Attribute? attribute;
-  int? constLvl;
+  String? specialisation;
   BaseTalent? baseTalent;
-  bool grouped;
 
   int currentLvl = 0;
   bool advancable = false;
 
-  Talent({required this.id, required this.name, this.attribute, this.constLvl, this.baseTalent, required this.grouped});
+  Talent({required this.id, required this.name, this.specialisation, this.baseTalent});
+
+  int? getMaxLvl() {
+    return baseTalent!.getMaxLvl();
+  }
+
+  @override
+  String toString() {
+    return "Talent (id=$id, name=$name)";
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {"ID": id, "NAME": name, "SPECIALISATION": specialisation, "BASE_TALENT": baseTalent?.id};
+  }
+}
+
+class BaseTalent extends DTO {
+  int id;
+  String name;
+  String description;
+  String source;
+  Attribute? attribute;
+  int? constLvl;
+
+  BaseTalent(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.source,
+      this.attribute,
+      this.constLvl});
 
   int? getMaxLvl() {
     if (attribute != null) {
@@ -25,33 +54,14 @@ class Talent extends DTO {
   }
 
   @override
-  String toString() {
-    return "Talent (id=$id, name=$name)";
-  }
-
-  @override
   Map<String, dynamic> toMap() {
     return {
       "ID": id,
       "NAME": name,
+      "DESCRIPTION": description,
+      "SOURCE": source,
       "MAX_LVL": attribute,
-      "CONST_LVL": constLvl,
-      "BASE_TALENT": baseTalent?.id,
-      "GROUPED": grouped ? 1 : 0
+      "CONST_LVL": constLvl
     };
-  }
-}
-
-class BaseTalent extends DTO {
-  int id;
-  String name;
-  String description;
-  String source;
-
-  BaseTalent({required this.id, required this.name, required this.description, required this.source});
-
-  @override
-  Map<String, dynamic> toMap() {
-    return {"ID": id, "NAME": name, "DESCRIPTION": description, "SOURCE": source};
   }
 }
