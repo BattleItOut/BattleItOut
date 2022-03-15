767aeaf242737a524b74187e43ec6acee12cde18
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-03-15 16:32:02 +0100

    #6 - added localise() extension function on String

--- lib/interface/screens/character_sheet_screen.dart
+++ lib/interface/screens/character_sheet_screen.dart
@@ -1,8 +1,7 @@
+import 'package:battle_it_out/entities_localisation.dart';
 import 'package:battle_it_out/interface/components/list_items.dart';
 import 'package:battle_it_out/localisation.dart';
 import 'package:battle_it_out/persistence/character.dart';
-import 'package:battle_it_out/persistence/entities/profession.dart';
-import 'package:battle_it_out/persistence/entities/race.dart';
 import 'package:flutter/material.dart';
 
 class CharacterSheetScreen extends StatefulWidget {
@@ -15,22 +14,6 @@ class CharacterSheetScreen extends StatefulWidget {
 }
 
 class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
-  String getRaceNameLocalised(Race race) {
-    if (race.subrace != null && race.name != race.subrace!.name) {
-      return "${AppLocalizations.of(context).localise(race.name)} (${AppLocalizations.of(context).localise(race.subrace!.name)})";
-    } else {
-      return AppLocalizations.of(context).localise(race.name);
-    }
-  }
-
-  String getProfessionNameLocalised(Profession profession) {
-    if (profession.career != null) {
-      return "${AppLocalizations.of(context).localise(profession.name)} (${AppLocalizations.of(context).localise(profession.career!.name)})";
-    } else {
-      return AppLocalizations.of(context).localise(profession.name);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,9 +25,9 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
         children: [
           CharacteristicListItem(
             children: [
-              ["Race:", getRaceNameLocalised(widget.character.race)],
-              ["Size:", AppLocalizations.of(context).localise(widget.character.race.size.name)],
-              ["Profession:", getProfessionNameLocalised(widget.character.profession)]
+              ["Race:", widget.character.race.getLocalName(context)],
+              ["Size:", widget.character.race.size.name.localise(context)],
+              ["Profession:", widget.character.profession.getLocalName(context)]
             ],
             columnTypes: const [
               CharacteristicType.name,
@@ -55,7 +38,7 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
           CharacteristicListItem(
             title: "Attributes",
             children: [
-              [for (var attribute in widget.character.attributes.values.where((attr) => attr.importance == 0)) AppLocalizations.of(context).localise(attribute.shortName)],
+              [for (var attribute in widget.character.attributes.values.where((attr) => attr.importance == 0)) attribute.shortName.localise(context)],
               [for (var attribute in widget.character.attributes.values.where((attr) => attr.importance == 0)) attribute.base.toString()],
               [for (var attribute in widget.character.attributes.values.where((attr) => attr.importance == 0)) attribute.advances.toString()],
               [for (var attribute in widget.character.attributes.values.where((attr) => attr.importance == 0)) attribute.getTotalValue().toString()]
@@ -65,7 +48,7 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
           CharacteristicListItem(
             children: [
               for (var attribute in widget.character.attributes.values.where((attr) => attr.importance > 0))
-              [AppLocalizations.of(context).localise(attribute.name), attribute.getTotalValue().toString()]
+              [attribute.name.localise(context), attribute.getTotalValue().toString()]
             ],
             columnTypes: const [
               CharacteristicType.name,
@@ -77,8 +60,8 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
             title: "Basic skills",
             children: [
               for (var skill in widget.character.skills.values.where((element) => !element.isAdvanced())) [
-                AppLocalizations.of(context).localise(skill.name),
-                AppLocalizations.of(context).localise(skill.getAttribute()!.shortName),
+                skill.name.localise(context),
+                skill.getAttribute()!.shortName.localise(context),
                 skill.getAttribute()!.getTotalValue().toString(),
                 skill.advances.toString(),
                 skill.getTotalValue().toString()
@@ -97,8 +80,8 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
               title: "Advanced skills",
               children: [
                 for (var skill in widget.character.skills.values.where((element) => element.isAdvanced())) [
-                  AppLocalizations.of(context).localise(skill.name),
-                  AppLocalizations.of(context).localise(skill.getAttribute()!.shortName),
+                  skill.name.localise(context),
+                  skill.getAttribute()!.shortName.localise(context),
                   skill.getAttribute()!.getTotalValue().toString(),
                   skill.advances.toString(),
                   skill.getTotalValue().toString()
@@ -117,7 +100,7 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
             title: "Talents",
             children: [
               for (var talent in widget.character.talents.values) [
-                AppLocalizations.of(context).localise(talent.name),
+                talent.name.localise(context),
                 talent.currentLvl.toString(),
                 talent.getMaxLvl()?.toString() ?? ""
               ]
@@ -133,7 +116,7 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
             title: "Armour",
             children: [
               for (var armour in widget.character.armour) [
-                AppLocalizations.of(context).localise(armour.name),
+                armour.name.localise(context),
                 armour.headAP.toString(),
                 armour.bodyAP.toString(),
                 armour.leftArmAP.toString(),
@@ -157,18 +140,18 @@ class _CharacterSheetScreenState extends State<CharacterSheetScreen> {
             title: "Weapons",
             children: [
               for (var weapon in widget.character.meleeWeapons) [
-                AppLocalizations.of(context).localise(weapon.name),
-                AppLocalizations.of(context).localise(weapon.length.name),
-                AppLocalizations.of(context).localise(weapon.skill!.specialisation!),
+                weapon.name.localise(context),
+                weapon.length.name.localise(context),
+                weapon.skill!.specialisation!.localise(context),
                 (weapon.damage + (widget.character.attributes[Character.strengthId]!.getTotalBonus())).toString(),
-                weapon.qualities.map((quality) => AppLocalizations.of(context).localise(quality.name)).join(", ")
+                weapon.qualities.map((quality) => quality.name.localise(context)).join(", ")
               ],
               for (var weapon in widget.character.rangedWeapons) [
-                AppLocalizations.of(context).localise(weapon.name),
+                weapon.name.localise(context),
                 weapon.range.toString(),
-                AppLocalizations.of(context).localise(weapon.skill!.specialisation!),
+                weapon.skill!.specialisation!.localise(context),
                 (weapon.damage + (weapon.strengthBonus ? (widget.character.attributes[Character.strengthId]!.getTotalBonus()) : 0)).toString(),
-                weapon.qualities.map((quality) => AppLocalizations.of(context).localise(quality.name)).join(", ")
+                weapon.qualities.map((quality) => quality.name.localise(context)).join(", ")
               ],
             ],
             columnTypes: const [
